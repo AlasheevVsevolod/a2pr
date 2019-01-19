a9899efb68d30ac7bf6d2616d729c252909b5257
Vsevolod <2007juggernaut@gmail.com>
2019-01-19 20:21:31 +0300

    task1

--- Advanced-Lesson-2-Inheritance/Practice.cs
+++ Advanced-Lesson-2-Inheritance/Practice.cs
@@ -37,6 +37,8 @@ namespace Advanced_Lesson_2_Inheritance
                     break;
 
                 case "3":
+                    newPrinter = new ImagePrinter(strToPrint, "newImage", 30, 5, 5);
+                    newPrinter.Print();
                     break;
 
                 default:
@@ -75,16 +77,56 @@ namespace Advanced_Lesson_2_Inheritance
     public class FilePrinter : Printer
     {
         private ConsoleColor _color;
-        private string fileName;
+        private string _fileName;
         public FilePrinter(string tmpStr, string tmpName) : base(tmpStr)
         {
-            fileName = tmpName;
+            _fileName = tmpName;
         }
         public override void Print()
         {
             Console.ForegroundColor = _color;
-            System.IO.File.AppendAllText($@"D:\{fileName}.txt", strToPrint);
+            System.IO.File.AppendAllText($@"D:\{_fileName}.txt", strToPrint);
             Console.ResetColor();
+        }
+    }
+
+    public class ImagePrinter : Printer
+    {
+        private string _fileName;
+        private int _fontSize, _x, _y;
+        private string _imgPath;
+        private Brush _fontColor;
+        private Font _font;
+
+        public ImagePrinter(string tmpStr, string tmpName, int tmpFontSize, int tmpX, int tmpY) : base(tmpStr)
+        {
+            _fileName = tmpName;
+            _imgPath = $@"D:\{_fileName}.bmp";
+
+            _fontSize = tmpFontSize;
+            _fontColor = Brushes.Crimson;
+            _font = new Font("Calibri", _fontSize);
+
+            _x = tmpX;
+            _y = tmpY;
+        }
+        public override void Print()
+        {
+            Bitmap newFile = new Bitmap(500, 500);
+
+            //Задаю фон изображению
+            for (int i = 0; i < newFile.Width; i++)
+            {
+                for (int y = 0; y < newFile.Height; y++)
+                {
+                    newFile.SetPixel(i, y, Color.White);
+                }
+            }
+
+            Graphics tmpG = Graphics.FromImage(newFile);
+
+            tmpG.DrawString(strToPrint, _font, _fontColor, _x, _y);
+            newFile.Save(_imgPath);
         }
     }
 }
